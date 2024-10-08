fc8c1e399384b0bb913ed4926c64ac3403664fad
banseoksuh <still3028@gmail.com>
2024-10-08 19:55:20 +0900

    [Refactor] [6-3] 리팩토링(2) - display 로직 이관

--- src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/locker/StudyCafeLockerPass.java
@@ -1,6 +1,9 @@
-package cleancode.studycafe.tobe.model;
+package cleancode.studycafe.tobe.model.pass.locker;
 
-public class StudyCafeLockerPass {
+import cleancode.studycafe.tobe.model.StudyCafePass;
+import cleancode.studycafe.tobe.model.StudyCafePassType;
+
+public class StudyCafeLockerPass implements StudyCafePass {
 
     private final StudyCafePassType passType;
     private final int duration;
@@ -24,6 +27,7 @@ public class StudyCafeLockerPass {
         return this.duration == duration;
     }
 
+    @Override
     public StudyCafePassType getPassType() {
         return passType;
     }
@@ -34,19 +38,6 @@ public class StudyCafeLockerPass {
 
     public int getPrice() {
         return price;
-    }
-
-    public String display() {
-        if (passType == StudyCafePassType.HOURLY) {
-            return String.format("%s시간권 - %d원", duration, price);
-        }
-        if (passType == StudyCafePassType.WEEKLY) {
-            return String.format("%s주권 - %d원", duration, price);
-        }
-        if (passType == StudyCafePassType.FIXED) {
-            return String.format("%s주권 - %d원", duration, price);
-        }
-        return "";
     }
 
 }
